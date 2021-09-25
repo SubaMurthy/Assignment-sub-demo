0c8e4a601166c090d529622960131bc296a13f17
SubaMurthy <81187458+SubaMurthy@users.noreply.github.com>
2021-09-26 01:44:49 +0530

    DAY 1 ASSIGNMENT SUBMITTED

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Title from './Title';
+import Pro from './Pro';
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Title/>    
+      <Pro/>
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ Pro.js
@@ -0,0 +1,14 @@
+import react, {Component} from 'react';
+
+class Pro extends Component {
+     
+    render() {
+        return(
+          <div>
+            <h1>computer</h1>
+          </div>
+        )
+    }
+}
+
+export default Pro;
